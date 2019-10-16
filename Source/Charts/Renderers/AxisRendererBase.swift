12b2afdb572683a911996d97d042a4e1965b6bc8
wangpeter <a735163935@gmail.com>
2019-10-16 16:12:34 +0800

    增加NaN判断，打印崩溃点

--- Source/Charts/Renderers/AxisRendererBase.swift
+++ Source/Charts/Renderers/AxisRendererBase.swift
@@ -97,7 +97,11 @@ open class AxisRendererBase: Renderer
         let labelCount = axis.labelCount
         let range = abs(yMax - yMin)
         
-        if labelCount == 0 || range <= 0 || range.isInfinite
+        if range.isNaN {
+            print("range is NaN")
+        }
+        
+        if labelCount == 0 || range <= 0 || range.isInfinite || range.isNaN
         {
             axis.entries = [Double]()
             axis.centeredEntries = [Double]()
